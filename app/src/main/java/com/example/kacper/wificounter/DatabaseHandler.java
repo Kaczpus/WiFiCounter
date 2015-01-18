9fa4d5dc30522fe0ce05938b4f4ddbe2d82d8e13
Kaczpus <kacperslys@gmail.com>
2015-01-18 22:04:54 +0100

    pierwszy commit

--- app/src/main/java/com/example/kacper/wificounter/DatabaseHandler.java
+++ app/src/main/java/com/example/kacper/wificounter/DatabaseHandler.java
@@ -4,4 +4,9 @@ package com.example.kacper.wificounter;
  * Created by Kacper on 2015-01-18.
  */
 public class DatabaseHandler {
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
 }
+
