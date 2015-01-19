eff443a2a34e629cb9d4adcf1431fb35b801ad1a
Kaczpus <kacperslys@gmail.com>
2015-01-19 22:53:53 +0100

    TROCHE BAZY DANYCH

--- app/src/main/java/com/example/kacper/wificounter/DatabaseHandler.java
+++ app/src/main/java/com/example/kacper/wificounter/DatabaseHandler.java
@@ -3,10 +3,54 @@ package com.example.kacper.wificounter;
 /**
  * Created by Kacper on 2015-01-18.
  */
-public class DatabaseHandler {
-    @Override
-    protected void finalize() throws Throwable {
-        super.finalize();
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHandler extends SQLiteOpenHelper{
+
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME  = "wificounter.db";
+    public static final String COLUMN_ID = "_id";
+    public static final String TABLE_WIFI = "WiFi";
+    public static final String WIFI_NAME = "WiFi_name";
+    public static final String TABLE_PROFILE = "Profile";
+    public static final String PROFIL_NAME = "Profil_name";
+    public static final String TABLE_HISTORY = "History";
+    public static final String DATE = "Date";
+    public static final String TIME_CONNECTION = "Time_connection";
+
+
+
+
+
+    public DatabaseHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String wifi =  "CREATE TABLE " + TABLE_WIFI + "(" + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT" + WIFI_NAME + "TEXT "
+        + ");";
+         String profil =  "CREATE TABLE " + TABLE_PROFILE + "(" + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT" + PROFIL_NAME + "TEXT"
+        + "wifi_id" + "INTEGER" + "FOREIGN KEY REFERENCES WiFi(wifi_id)"  + ");";
+        String history = "CREATE TABLE " + TABLE_HISTORY + "(" + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT" + DATE + "TEXT"
+         + TIME_CONNECTION + "INTEGER"
+             + "_id_profile" + "INTEGER" + "FOREIGN KEY REFERENCES" + PROFIL_NAME + "(_id_profile)"  + ");";
+
+        db.execSQL(wifi);
+        db.execSQL(profil);
+        db.execSQL(history);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_HISTORY);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PROFILE);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_WIFI);
+        onCreate(db);
+    }
+
+
 }
 
--- app/src/main/java/com/example/kacper/wificounter/MainActivity.java
+++ app/src/main/java/com/example/kacper/wificounter/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.kacper.wificounter;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
-
+    public final static String EXTRA_MESSAGE = "com.example.kacper.wificounter.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +23,14 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     @Override
