f62092cae21da2df5342c9600c8263ee08822b4b
Hunt <huntersky93@gmail.com>
2015-01-25 00:33:40 +0100

    podstawy do wifi

--- app/src/main/java/com/example/kacper/wificounter/WiFi.java
+++ app/src/main/java/com/example/kacper/wificounter/WiFi.java
@@ -1,10 +1,55 @@
 package com.example.kacper.wificounter;
 
+import android.content.Context;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+
 /**
  * Created by Kacper on 2015-01-18.
  */
 public class WiFi {
-    private int _id;
+
+    WifiManager wifiManager;
+    WifiInfo wifiInfo;
+    String WifiName;
+    public WiFi(Context context)
+    {
+        wifiManager = (WifiManager)context.getSystemService(Context.WIFI_SERVICE);
+
+    }
+
+    public void connectToWiFi()
+    {
+        wifiManager.setWifiEnabled(true);
+    }
+
+    public void disconnectWiFi()
+    {
+        wifiManager.setWifiEnabled(false);
+    }
+
+    public String getWiFiSSID()
+    {
+        if(wifiManager.isWifiEnabled())
+        {
+            wifiInfo = wifiManager.getConnectionInfo();
+            WifiName = wifiInfo.getSSID();
+
+            return WifiName;
+        }
+
+        else
+        {
+            return "Please enable WiFi first";
+        }
+    }
+
+    public boolean isWiFiConnected()
+    {
+        if(wifiManager.isWifiEnabled()) return true;
+        else return false;
+    }
+    /*private int _id;
     private String _wifiname;
 
     public int get_id() {
@@ -28,5 +73,11 @@ public class WiFi {
     }
 
     public WiFi() {
-    }
+    }()*/
+
+
+
+
+
+
 }
