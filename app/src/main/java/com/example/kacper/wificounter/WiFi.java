2223c93a02a73441f0ee6a08ee2fdedf4e7cfb69
Hunt <huntersky93@gmail.com>
2015-01-25 00:34:35 +0100

    podstawy do wifi

--- app/src/main/java/com/example/kacper/wificounter/WiFi.java
+++ app/src/main/java/com/example/kacper/wificounter/WiFi.java
@@ -49,31 +49,6 @@ public class WiFi {
         if(wifiManager.isWifiEnabled()) return true;
         else return false;
     }
-    /*private int _id;
-    private String _wifiname;
-
-    public int get_id() {
-        return _id;
-    }
-
-    public void set_id(int _id) {
-        this._id = _id;
-    }
-
-    public String get_wifiname() {
-        return _wifiname;
-    }
-
-    public void set_wifiname(String _wifiname) {
-        this._wifiname = _wifiname;
-    }
-
-    public WiFi(String _wifiname) {
-        this._wifiname = _wifiname;
-    }
-
-    public WiFi() {
-    }()*/
 
 
 
