7e215a339766ef796fe02d83709eef11411e76bc
Kaczpus <kacperslys@gmail.com>
2015-01-25 21:44:07 +0100

    Dialogi

--- app/src/main/java/com/example/kacper/wificounter/MainActivity.java
+++ app/src/main/java/com/example/kacper/wificounter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.kacper.wificounter;
 
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -30,9 +32,9 @@ public class MainActivity extends ListActivity {
         openAndQueryDatabase();
         displayResultList();
         ProfileHandler = new ProfileHandler(getBaseContext());
-        Input = (EditText) findViewById(R.id.Input);
+       /* Input = (EditText) findViewById(R.id.Input);
         Text = (TextView) findViewById(R.id.Text);
-        Input.setText("");
+        Input.setText("");*/
         setListAdapter(new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, result));
         ListView lv = getListView();
@@ -51,32 +53,76 @@ public class MainActivity extends ListActivity {
                     myIntent.putExtra("profile",profile.get_id());
                     startActivityForResult(myIntent, 0);
 
-
-
-
             }
         });
 
 
     }
 
-
-
-
-
-
     public void addButtonClicked(View view) {
-        Profile profile = new Profile(Input.getText().toString());
-        ProfileHandler.addItem(profile);
-        result.add(profile.get_profilname());
+       // Profile profile = new Profile(Input.getText().toString());
+      //  ProfileHandler.addItem(profile);
+      //  result.add(profile.get_profilname());
+      //  displayResultList();
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Adding");
+        alert.setMessage("Please enter name of your profile");
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String value = input.getText().toString();
+                Profile profile = new Profile(value);
+                ProfileHandler.addItem(profile);
+                result.add(profile.get_profilname());
+                displayResultList();
+            }
+       });
+       alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+       });
+
+        alert.show();
         displayResultList();
-    }
+}
 
     public void deleteButtonClicked(View view) {
-        String input = Input.getText().toString();
+        /*String input = Input.getText().toString();
         ProfileHandler.deleteItem(input);
         result.remove(input);
+        displayResultList();*/
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+        alert.setTitle("Deleting");
+        alert.setMessage("Please enter name of your profile to delete");
+
+        final EditText input = new EditText(this);
+        alert.setView(input);
+
+        alert.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String value = input.getText().toString();
+                ProfileHandler.deleteItem(value);
+                result.remove(value);
+                displayResultList();
+            }
+        });
+        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int whichButton) {
+                // Canceled.
+            }
+        });
+
+        alert.show();
         displayResultList();
+
 
     }
 
